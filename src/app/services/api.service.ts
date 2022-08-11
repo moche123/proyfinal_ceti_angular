2726749031a099c6c4874baca6b1b881cc74ae47
moche123 <mochemiguel@gmail.com>
2022-08-10 21:48:30 -0500

    cambios prod  cambio link

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { of, Observable } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
